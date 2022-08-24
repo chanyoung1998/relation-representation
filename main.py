193ea5290f20470f2fca7a86fb50717f50b40c7c
Sabin <50198431+sabin5105@users.noreply.github.com>
2022-08-24 22:27:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from korre.korre import KorRE
 from cls_model import *
 from relation_extraction_model import predict
+from chk_sentiment import check_sentiment_absence
 
 
 korre = KorRE()
-relations = {}
+relations_emo = {}
+relation = {}
 
-sentence = "찬영이에게 사빈이는 눈물을 보였습니다. 찬영이는 그런 사빈이를 위로해줬습니다. 사빈이는 감사한 마음을 담아 찬영이에게 선물을 줬습니다."
 
-for sent in sentence.split('.')[:-1]:
-    ner = korre.ner_sub_obj(sent)
-    sub_obj = result(ner, emo_predict(sent))
+def main():
+    sentence = "사빈이는 연인, 영심이한테 사랑한다고 말했다. 그러자 영심이는 감동을 받았는지 사빈이에게 눈물을 보였다. 찬영이는 그런 사빈이를 위로해줬습니다. 사빈이는 감사한 마음을 담아 찬영이에게 선물을 줬습니다."
 
-    if f'{sub_obj[0]}-{sub_obj[1]}' not in relations.keys():
-        relations[f'{sub_obj[0]}-{sub_obj[1]}'] = sub_obj[2]
-    else:
-        relations[f'{sub_obj[0]}-{sub_obj[1]}'] += "/" + sub_obj[2]
+    for sent in sentence.split('.')[:-1]:
+        print(check_sentiment_absence(sent))
+        if check_sentiment_absence(sent)[0] < 1 and check_sentiment_absence(sent)[1] > -1:
+            continue
+        ner = korre.ner_sub_obj(sent)
+        sub_obj = result(ner, emo_predict(sent))
 
-print(relations)
+        if f'{sub_obj[0]}-{sub_obj[1]}' not in relations_emo.keys():
+            relations_emo[f'{sub_obj[0]}-{sub_obj[1]}'] = sub_obj[2]
+        else:
+            relations_emo[f'{sub_obj[0]}-{sub_obj[1]}'] += "/" + sub_obj[2]
+        
+        tagged_sent = korre.ner_tagged(sent)
+        relation[(korre.ner_sub_obj(sent)[0][0], korre.ner_sub_obj(sent)[1][0])] = predict(tagged_sent)
 
-# ------------------------------------------ #
-sent = "찬영이에게 동생, 사빈이는 눈물을 보였습니다"
-tagged_sent = korre.ner_tagged(sent)
-print(predict(tagged_sent))
 
-#TODO
+    print(relations_emo)
+    print(relation)
+
+
+if __name__ == '__main__':
+    main()
+    
